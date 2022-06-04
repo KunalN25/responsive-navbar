c0b31b4d94b13fd1e9474e6d7e84866d67457765
knalawade <knalawade@vmware.com>
2022-06-04 20:35:32 +0530

    Edited with no Javascript implementation

--- index.js
+++
@@ -1,7 +0,0 @@
-function onMenuClick() {
-    var navbar = document.getElementById('navigation-bar');
-    var responsive_class_name = 'responsive'
-    
-    navbar.classList.toggle(responsive_class_name)
-
-}
