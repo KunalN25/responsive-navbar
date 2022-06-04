1573c4822ae49be651af88f1f05a38fbb416bf43
knalawade <knalawade@vmware.com>
2022-06-04 20:33:43 +0530

    Added responsive navbar

---
+++ index.js
@@ -0,0 +1,7 @@
+function onMenuClick() {
+    var navbar = document.getElementById('navigation-bar');
+    var responsive_class_name = 'responsive'
+    
+    navbar.classList.toggle(responsive_class_name)
+
+}
